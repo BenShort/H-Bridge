8d2e8da45e1f9badcc7dd021ef3f91af6c0b37f4
BenShort <benedictnjshort@gmail.com>
2022-07-15 09:34:21 +0100

    fixing serial delay

--- Code/Functional Examples/Serial_input_test/serial_input/serial_input.ino
+++ Code/Functional Examples/Serial_input_test/serial_input/serial_input.ino
@@ -1,22 +1,29 @@
 //variables
 String value;
 float t;
+int myTimeout = 10;
+int long t1, t2, t3;
 
 void setup(){
     Serial.begin(9600);
+    Serial.setTimeout(myTimeout);
     Serial.println("Setup successful");
 }
 
 void loop(){
     if(Serial.available()){
+        t1 = millis();
         value = Serial.readString();
+        t2 = millis();
+        t3 = t2 - t1;
+        Serial.println(String(t3));
         t = value.toFloat();
-        Serial.println(value);
+        //Serial.println(value);
         Serial.println(String(t));
     }
-    else{
-        Serial.println("nothing new");
-        delay(1000);
-    }
+    //else{
+    //    Serial.println("nothing new");
+    //    delay(1000);
+    //}
     
 }
